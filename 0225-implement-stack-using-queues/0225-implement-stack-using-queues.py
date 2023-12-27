b4c43f248077f4bca7499d9608f618f2ff1c301f
Hannah L <106502799+ht-l1@users.noreply.github.com>
2023-12-27 14:06:36 -0500

    Time: 40 ms (41.39%), Space: 17.3 MB (6.55%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,26 @@
+class MyStack:
+	# initializing the stack
+    def __init__(self):
+        self.q = deque()
+	
+	# push: append it to the right side of the queue
+    def push(self, x: int) -> None:
+        self.q.append(x)
+	
+	# take elements out
+	# in a queue, you can only remove from the left
+    def pop(self) -> int:
+        for i in range(len(self.q) - 1):
+            self.push(self.q.popleft())
+        return self.q.popleft()
+	
+	# looking at the top element without taking out
+    def top(self) -> int:
+        for i in range(len(self.q) - 1):
+            self.push(self.q.popleft())
+        res = self.q[0]
+        self.push(self.q.popleft())
+        return res
+		
+    def empty(self) -> bool:
+        return len(self.q) == 0
