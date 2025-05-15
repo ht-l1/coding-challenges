edbdff1d70a1aed5a16c4f0b5304dce445236de2
Hannah L <106502799+ht-l1@users.noreply.github.com>
2025-05-15 18:37:22 -0400

    Time: 24 ms (17%), Space: 31.6 MB (53.97%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,19 +1,8 @@
-  # use HashSet to keep track of unique elements while iterating
-
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-    
-    # HashSet Initialization creates an empty set named 'hashset'. This set will be used to keep track of unique elements encountered in the array.
-    # set() is a data type representing an unordered collection of unique elements
-        hashset = set()
-    
-        # Iterating Through the Array:
-        for n in nums:
-            
-            # Checking for Duplicates:
-            if n in hashset:
+        seen = set()
+        for num in nums:
+            if num in seen:
                 return True
-            
-            # Adding to HashSet:
-            hashset.add(n)
+            seen.add(num)
         return False
