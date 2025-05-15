9d3a2ae523a438d12608455e3e7938b058448bcc
Hannah L <106502799+ht-l1@users.noreply.github.com>
2025-05-15 19:47:57 -0400

    Time: 15 ms (42.41%), Space: 18.5 MB (19.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,9 +4,4 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        sorted_s = sorted(s)
-        sorted_t = sorted(t)
-
-        if sorted_s == sorted_t:
-            return True
-        return False
+        return sorted(s) == sorted(t)
