cd552bbcc9041e994314e26eb5bc9a91228b0381
Hannah L <106502799+ht-l1@users.noreply.github.com>
2025-05-16 16:22:38 -0400

    Time: 6 ms (55.68%), Space: 21.4 MB (40.16%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+
+        sorted_items = sorted(count.items(), key=lambda x: x[1], reverse=True)
+
+        result = [item[0] for item in sorted_items[:k]]
+        return result
