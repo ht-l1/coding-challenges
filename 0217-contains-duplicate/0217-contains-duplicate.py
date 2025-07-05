2cc6a030504a0aeac29867272fe77c3a95caef7d
Hannah L <106502799+ht-l1@users.noreply.github.com>
2025-07-05 12:54:01 -0400

    Time: 11 ms (76.34%), Space: 31.5 MB (82.73%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
+        # input: integer array nums
+        # output: true if appears more than once
+        # output: false if all distinct
+        # no constaints
+
+        # use hash map, and see if any value hits 2
+        seen_pair = set()
+
+        for i in nums:
+            if i in seen_pair:
                 return True
-            seen.add(num)
+            seen_pair.add(i)
+
         return False
